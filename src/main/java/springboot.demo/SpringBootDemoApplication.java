913565f9596af025e06a7c0e5576631bbef0e6aa
root <root@lying>
2022-11-17 15:29:29 +0800

    commit

---
+++ src/main/java/springboot.demo/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: SpringBootDemo
+ * @author: Lying
+ * @description: TODO
+ * @date: 2022/11/17 上午10:31
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
